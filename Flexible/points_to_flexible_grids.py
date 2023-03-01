f765ddf029ba52e34c2e03baf1fb0507e2694779
Funso Oje <funsooje@gmail.com>
2023-03-01 13:18:08 -0800

    Update points_to_flexible_grids.py
    Generating first level of grid

--- Flexible/points_to_flexible_grids.py
+++ Flexible/points_to_flexible_grids.py
@@ -13,6 +13,7 @@ Assumptions:
 import math
 import numpy as np
 import pandas as pd
+from numba import jit
 
 # Global Constants
 
@@ -69,70 +70,57 @@ class FGP:
         print("Actual Grid Size: {}km by {}km".format(np.round(self.lon_per_deg*self.delta_lon[0], 3), np.round(self.lat_per_deg*self.delta_lat[0], 3)))
         print("-----------------------------------------")
 
-    def generateGrids(self, locations: pd.DataFrame, existingGrids: pd.DataFrame):
-        """
-        Pairs the locations with the existing grids if they already fall within the grid
-        Create new grids if location does not within an existing grid
-        Returns an array of all levels of data grid first array containing the original locations
-        """
-
-        # Check that locations contains the right columns
-        if 'latitude' not in locations.columns:
-            raise ValueError("Locations dataframe has no latitude column")
-        if 'longitude' not in locations.columns:
-            raise ValueError("Locations dataframe has no longitude column")
-
-        parentGrids = pd.DataFrame({'id': pd.Series(dtype='int'),
-                                    'centerLon': pd.Series(dtype='float'),
-                                    'centerLat': pd.Series(dtype='float'),
-                                    'upperLat': pd.Series(dtype='float'),
-                                    'lowerLat': pd.Series(dtype='float'),
-                                    'upperLon': pd.Series(dtype='float'),
-                                    'lowerLon': pd.Series(dtype='float'),
-                                    'level': pd.Series(dtype='int')})
-
+    def generate(self, data: pd.DataFrame, existingGrids: pd.DataFrame):
+        locations = data.copy()
+        locs_array = locations[['longitude','latitude']].to_numpy()
+        loclen = len(locs_array)
+        parentIDs = np.full(loclen, -1)
+        parent_lon = np.empty(loclen)
+        parent_lat = np.empty(loclen)
+        parent_lon[:] = np.nan
+        parent_lat[:] = np.nan
+        
+        parents_array = np.array([])
+        new_parents = np.array([])
         if existingGrids is not None:
-            if ('id' not in existingGrids.columns) or ('centerLon' not in existingGrids.columns) or ('centerLat' not in existingGrids.columns) or ('level' not in existingGrids.columns):
-                raise ValueError("Existing dataframe should have id, centerLon, centerLat and level columns")
-            parentGrids = existingGrids
-            parentGrids = self._updateParentBoudaries(parentGrids)
+            # parentGrids = self._updateParentBoudaries(existingGrids, 1)
+            parents_array = existingGrids[['id', 'centerLon', 'centerLat', 'upperLon', 'lowerLon', 'upperLat', 'lowerLat']].to_numpy()
 
+        for i in range(len(locs_array)):
+            # print("---")
+            each_lon = locs_array[i][0]
+            each_lat = locs_array[i][1]
+            which, parentID = self._findParent(each_lon, each_lat, parents_array, new_parents)
+            
+            if which == "parent":
+                parentIDs[i] = parents_array[parentID][0]
+                parent_lon[i] = parents_array[parentID][1]
+                parent_lat[i] = parents_array[parentID][2]
+            elif which == "new":
+                parentIDs[i] = -1
+                parent_lon[i] = new_parents[parentID][1]
+                parent_lat[i] = new_parents[parentID][2]
+            else: # totally fresh one
+                # create new grid
+                upperLon, lowerLon, upperLat, lowerLat = self._getNewGridParams(each_lon, each_lat, 1)
+                if len(new_parents) == 0:
+                    # new one
+                    new_parents = np.array([[0, each_lon, each_lat, upperLon, lowerLon, upperLat, lowerLat]])
+                else:
+                    # append
+                    new_parents = np.concatenate((
+                        new_parents,
+                        np.array([[0, each_lon, each_lat, upperLon, lowerLon, upperLat, lowerLat]])
+                    ), axis = 0)
 
-        result = []
+                parentIDs[i] = -1
+                parent_lon[i] = each_lon
+                parent_lat[i] = each_lat
 
-        # Level 0
-        data_copy = locations.copy()
-        data_copy['parentID'] = 0
-        data_copy['parent_lon'] = None
-        data_copy['parent_lat'] = None
-        data_copy['level'] = 0
-
-        # Iterative Method
-        data_copy['parentID'] = data_copy.apply(lambda row: self._getParent(row, parentGrids, 1), axis = 1)
-
-        newParents = pd.DataFrame({'id': pd.Series(dtype='int'),
-                                    'centerLon': pd.Series(dtype='float'),
-                                    'centerLat': pd.Series(dtype='float'),
-                                    'upperLat': pd.Series(dtype='float'),
-                                    'lowerLat': pd.Series(dtype='float'),
-                                    'upperLon': pd.Series(dtype='float'),
-                                    'lowerLon': pd.Series(dtype='float'),
-                                    'level': pd.Series(dtype='int')})
-        for row_id,row in data_copy.iterrows():
-            if row['parentID'] > 0:
-                continue
-            #generate parent for existing
-            centerLon, centerLat, newParents = self._createNewParent(row, newParents, 1)
-            data_copy.loc[row_id, 'parent_lon'] = centerLon
-            data_copy.loc[row_id, 'parent_lat'] = centerLat
-
-        data_copy.parentID = data_copy.parentID.astype('int')
-        result.append(data_copy)
-
-        # TODO: repeat for the remaining levels
-        # TODO: parent should be given in an array of levels too
-        return result
-
+        locations['parentID'] = parentIDs
+        locations['parent_lon'] = parent_lon
+        locations['parent_lat'] = parent_lat
+        return locations
 
     def _getDeltas(self, silent: bool):
         """
@@ -155,67 +143,57 @@ class FGP:
             self.delta_lon.append(np.round((level_size / self.lon_per_deg),self.measurement_sensitivity))
             self.delta_lat.append(np.round((level_size / self.lat_per_deg),self.measurement_sensitivity))
 
-    def _getParent(self, row, parents, parent_level):
-        if (row['parentID'] > 0):
-            return
-        lat = row['latitude']
-        lon = row['longitude']
 
-        arr = np.where((parents['upperLat'] >= lat) & 
-                        (parents['lowerLat'] <= lat) & 
-                        (parents['lowerLon'] <= lon) & 
-                        (parents['lowerLon'] <= lon) &
-                        (parents['level'] == parent_level))[0]
-        if len(arr) > 0:
-            return parents.loc[arr[0]].id
-        else:
-            return 0
-
-    def _createNewParent(self, row, parents, parent_level):
-        lat = row['latitude']
-        lon = row['longitude']
-        arr = np.where((parents['upperLat'] >= lat) & 
-                    (parents['lowerLat'] <= lat) & 
-                    (parents['lowerLon'] <= lon) & 
-                    (parents['lowerLon'] <= lon) &
-                    (parents['level'] == parent_level))[0]
-        if len(arr) > 0:
-            parent = parents.loc[arr[0]]
-            return parent.centerLon, parent.centerLat, parents
-        else:
-            # really new parent
-            upperLon, lowerLon, upperLat, lowerLat = self.getNewGridParams(lon, lat, parent_level)
-            a = {'centerLon': lon, 'centerLat': lat, 'upperLon': upperLon, 'upperLat': upperLat, 'lowerLon': lowerLon, 'lowerLat': lowerLat, 'level': parent_level}
-            parents = pd.concat([parents, pd.DataFrame(a, index = [0])], ignore_index=True)
-            return lon, lat, parents
-
-
-    def getNewGridParams(self, lon, lat, level):
+    def _getNewGridParams(self, lon, lat, level):
         upperLat = lat + self.delta_lat[level-1]
         lowerLat = lat - self.delta_lat[level-1]
         upperLon = lon + self.delta_lon[level-1]
         lowerLon = lon - self.delta_lon[level-1]
         return upperLon, lowerLon, upperLat, lowerLat
 
+    def _findParent(self, lon, lat, parents_array: np.array, new_parents: np.array):
+        # check parents array
+        if len(parents_array) > 0:
+            parent_match = np.intersect1d(
+                    np.intersect1d(
+                        np.where(parents_array[:,3] >= lon)[0], 
+                        np.where(parents_array[:,4] <= lon)[0]), # lon matches
+                    np.intersect1d(
+                        np.where(parents_array[:,5] >= lat)[0], 
+                        np.where(parents_array[:,6] <= lat)[0]) # lat matches
+                ) # both match
+            if len(parent_match) > 0:
+                return "parent", parent_match[0]
+        
+        # check new parents
+        if len(new_parents) > 0:
+            parent_match = np.intersect1d(
+                    np.intersect1d(
+                        np.where(new_parents[:,3] >= lon)[0], 
+                        np.where(new_parents[:,4] <= lon)[0]), # lon matches
+                    np.intersect1d(
+                        np.where(new_parents[:,5] >= lat)[0], 
+                        np.where(new_parents[:,6] <= lat)[0]) # lat matches
+                ) # both match
+            if len(parent_match) > 0:
+                return "new", parent_match[0]
+        
+        # fresh one
+        return "fresh", -1
+        return -1
     
-    def _updateParentBoudaries(self, parents: pd.DataFrame):
-        parents['upperLat'] = parents.centerLat + self.delta_lat[0]
-        parents['lowerLat'] = parents.centerLat - self.delta_lat[0]
-        parents['upperLon'] = parents.centerLon + self.delta_lon[0]
-        parents['lowerLon'] = parents.centerLon - self.delta_lon[0]
-        for i in range(1, self.levels):
-            parents['upperLat'] = np.where(parents.level == i+1, parents.centerLat + self.delta_lat[i], parents['upperLat'])
-            parents['lowerLat'] = np.where(parents.level == i+1, parents.centerLat + self.delta_lat[i], parents['lowerLat'])
-            parents['upperLon'] = np.where(parents.level == i+1, parents.centerLon + self.delta_lat[i], parents['upperLon'])
-            parents['lowerLon'] = np.where(parents.level == i+1, parents.centerLon + self.delta_lat[i], parents['lowerLon'])
-
+    def _updateParentBoudaries(self, parents: pd.DataFrame, parent_level):
+        parents['upperLat'] = parents['centerLat'] + self.delta_lat[parent_level-1]
+        parents['lowerLat'] = parents['centerLat'] - self.delta_lat[parent_level-1]
+        parents['upperLon'] = parents['centerLon'] + self.delta_lon[parent_level-1]
+        parents['lowerLon'] = parents['centerLon'] - self.delta_lon[parent_level-1]
+        
         return parents
 
 
-
-# FORMER method
-# run through batch and find matches
-# run through batch and create new parents
-# push found batches and parents in one go
-# rerun to link unfound batches with pushed parents
+# TODOS:
+# start fresh so you can create new parents with no padding
+# this is just for one level - complete for all levels
+# parents supplied to generate should be an array of levels
+# in the main script figure out logic for handling levels and saving to the database
         
